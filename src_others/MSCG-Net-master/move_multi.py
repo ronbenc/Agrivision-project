1eb4e11c43c07292427a93b342e5e518649ad670
ronbenc <ronbenc@campus.technion.ac.il>
2022-09-25 13:04:26 +0300

    Add database filtering script

---
+++ src_others/MSCG-Net-master/move_multi.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+DIR = '/home/ronbenc/Agrivision-project/dataset/train/images/nir'
+multi_dir = '/home/ronbenc/Agrivision-project/dataset/train/images/train_multi_nir'
+os.mkdir(multi_dir)
+count=0
+total_count=0
+for parent, dirnames, filenames in os.walk(DIR):
+    for fn in filenames:
+        total_count +=1 
+        if "multi" in fn:
+            print(fn)
+            print(os.path.join(parent, fn))
+            shutil.move(os.path.join(parent, fn), os.path.join(multi_dir, fn))
+            count+= 1
+print("count: " , count)
+print("total_count: " , total_count)
+
+DIR = '/home/ronbenc/Agrivision-project/dataset/val/images/nir'
+multi_dir = '/home/ronbenc/Agrivision-project/dataset/val/images/train_multi_nir'
+os.mkdir(multi_dir)
+count=0
+total_count=0
+for parent, dirnames, filenames in os.walk(DIR):
+    for fn in filenames:
+        total_count +=1 
+        if "multi" in fn:
+            print(fn)
+            print(os.path.join(parent, fn))
+            shutil.move(os.path.join(parent, fn), os.path.join(multi_dir, fn))
+            count+= 1
+print("count: " , count)
+print("total_count: " , total_count)
+
